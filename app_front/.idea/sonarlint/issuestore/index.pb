50b52372da9cdf8d84257b5812d7bc4d01ef0677
shanshancao <shanshan.cao@thalesdigital.io>
2019-12-12 15:51:34 +0100

    add css module

---
+++ app_front/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+gatsby-config.js,4/f/4f261c6e46142df5e9a7303296b3fca674d719a9
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+I
+src/components/layout.css,a/2/a2f9e832e5c6cb7482d0a6f170ad4de915b8579c
+H
+src/components/layout.js,2/a/2a0d7b3d9bc08b1aca1670d3640c4f2263da08ea
+H
+src/components/header.js,0/c/0c051dedc4e0bbd1604c4eff9143e048b5b56fcc
+E
+src/components/seo.js,8/2/82f038d290bd9865a0fa942f8e9860022807f874
--- app_front/src/pages/index.js
+++ app_front/src/pages/index.js
@@ -4,18 +4,53 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Container from "../components/container";
+import userStyles from "../styles/about-css-modules.module.css";
+
+const User = props => (
+    <div className={userStyles.user}>
+        <img src={props.avatar} className={userStyles.avatar} alt="" />
+        <div className={userStyles.description}>
+            <h2 className={userStyles.username}>{props.username}</h2>
+            <p className={userStyles.excerpt}>{props.excerpt}</p>
+        </div>
+    </div>
+)
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Hi shanshan</h1>
+    <p>Welcome to your new website.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+
+      {/*Component-scoped CSS*/}
+      <Container>
+          <h1>About CSS Modules</h1>
+          <p>CSS Modules are cool</p>
+          <User>
+              <User
+                  username="Jane Doe"
+                  avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+                  excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+              />
+          </User>
+          <User>
+              <User
+                  username="Bob Smith"
+                  avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+                  excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+              />
+          </User>
+      </Container>
+
   </Layout>
 )
+
+
 
 export default IndexPage
